b3e614113f52f0c36a96af5ae357f49070137fb4
Marcin Kurek <md.kurek@gmail.com>
2020-04-02 11:16:52 +0200

    refactoring

--- rpl_modify.py
+++ rpl_modify.py
@@ -1,30 +1,58 @@
+#!/usr/bin/env python
+
+"""
+rpl_modify is intended to run as a standalone script. 
+
+Here's what it does:
+
+1. It takes up to 3 parameters (prefix, action, and optional uplink)
+2. Updates its internal representation of system state (stored in state.json file)
+3. Based on current system state generates RPL prefix-set
+4. Pushes the RPL prefix-set to devices specified in inventory file (inventory.json) using NETCONF
+
+"""
+
+__author__ = "Marcin Kurek"
+__version__ = "0.1"
+__maintainer__ = "Marcin Kurek"
+__email__ = "m.kurek@3s.pl"
+__status__ = "Development"
+
 import json
 import getopt
 import sys
 import logging
 import time
-from verify import get_advertised_prefixes
+import os.path
+
 from jinja2 import Template
 from ncclient import manager
 
 def load_inventory():
 	"""
-	Load inventory from file. 
-	Inventory contains metadata of all BGP sessions we are going to modify.
+	Load inventory from file. Inventory contains metadata of all BGP sessions we are going to modify.
+    
+	Parameters:
+	none
 
-	Returns: dict containing inventory
+	Returns: 
+	dict: inventory
+	
 	"""
+	
 	try:
 		with open("inventory.json", encoding="utf-8") as f:
 			input = json.load(f)
 		return input
-	except:
-		pass
+	except (OSError, IOError) as e:
+		print(e)
+		sys.exit(1)
 
 def read_state():
 
     """
-    Odczytuje stan z pliku.
+    Reads state from a file.
+	State is a current representation of the system, i.e. which prefixes should be advertised.
 
     """
     with open("state.json", encoding="utf-8") as f:
@@ -34,7 +62,7 @@ def read_state():
 def write_state(state:json):
 
     """
-    Zapisuje stan do pliku.
+    Writes state to a file.
 
     :param state:
     :return:
@@ -44,16 +72,19 @@ def write_state(state:json):
         f.write(json.dumps(state, indent=4, sort_keys=True))
 
 def update_state(prefix: str, action:str) -> str:
+
     logging.debug('Updating state for %s : %s', prefix, action)
     current_state = read_state()
     current_state[prefix] = action
     write_state(current_state)
+    
     return current_state
 
 def deploy(rpl_config:str, device:str):
 
     """
-    Laduje konfiguracje na wskazany router za pomoca NETCONF.
+    Deploys a RPL snippet to a device.
+
     :param rpl_config:
     :return:
     """
@@ -65,11 +96,12 @@ def deploy(rpl_config:str, device:str):
 def generate_rpl(upstream:str, current_state:dict, template_path:str):
 
     """
-    Generuje nowa RPL na podstawie template'u.
+    Generates a RPL snippet based on current system state.
     :param upstream:
     :param current_state:
     :return:
     """
+
     src = []
     for k,v in current_state.items():
         if v == '1':
@@ -93,10 +125,20 @@ def generate_prefix_set(current_state:dict, template_path:str):
 def main():
 	
 	# TODO: Lock file
-	# TODO: Logging - dlaczego pozostale moduly podpinaja sie pod logi?
 
 	logging.basicConfig(level=logging.INFO, filename='rpl_modify.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
 	logging.info("rpl_modify starting...")
+
+	LOCKFILE = '/tmp/rpl_modify.lock'
+
+	if os.path.exists(LOCKFILE):
+		logging.info("another instance of the script running, terminating")
+		print("another instance of the script is running, terminating")
+		sys.exit(1)
+	else:
+		with open(LOCKFILE, 'w') as f:
+			pass
+
 	argv = sys.argv[1:]
 	
 	try:
@@ -104,6 +146,7 @@ def main():
 		if len(opts) == 0 or len(opts) > 3:
 			print('Usage: rpl_modify.py -p <prefix> -a <action>')
 			logging.error("Incorrect number of parameters given, terminating...")
+			os.remove(LOCKFILE)
 			sys.exit(1)
 		else:
 			for opt, arg in opts:
@@ -113,15 +156,20 @@ def main():
 					action = arg
 				if opt in ('-u', '--upstream'):
 					upstream = arg
+				else:
+					upstream = None
 	except getopt.GetoptError:
 		print ('Usage: rpl_modify.py -p <prefix> -a <action> -u upstream')
 
-    # Odczytujemy z pliku aktualny stan systemu i aktualizujemy go zgodnie z przekazanymi parametrami
-	logging.info("We will be modifying advertisement for {}".format(prefix))
+    # Load inventory
+
 	inventory = load_inventory()
+
+	# Update system state
+
 	current_state = update_state(prefix, action)
 	
-	# Generujemy prefix-set dla aktualnego stanu systemu
+	# Generate prefix-set according to current system state
 	
 	prefix_set = generate_prefix_set(current_state, 'templates/template_prefix_set.html')
 
@@ -135,9 +183,10 @@ def main():
 	else:
 		for key, value in inventory.items():
 			logging.info("Performing run for {}".format(key))
-			logging.info("Deploying to device {}".format(value['device'])
+			logging.info("Deploying to device {}".format(value['device']))
 			result = deploy(prefix_set, value['device'])
 
+	os.remove(LOCKFILE)
 
 if __name__ == "__main__":
     main()
